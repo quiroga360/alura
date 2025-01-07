659eb7c591c168e1b0d8ef09d4f67d04a6492524
Pablo Quiroga <quirogalogins@gmail.com>
2025-01-07 08:33:35 -0300

    Clicando no botão; Conectar JS com HTML; Buscar um elemento

---
+++ 05-javascript/aula-01/main.js
@@ -0,0 +1,11 @@
+const pomBtn = document.querySelector(".tecla_pom");
+const clapBtn = document.querySelector(".tecla_clap");
+const timBtn = document.querySelector(".tecla_tim");
+
+const puffBtn = document.querySelector(".tecla_puff");
+const splashBtn = document.querySelector(".tecla_splash");
+const toimBtn = document.querySelector(".tecla_toim");
+
+const pshBtn = document.querySelector(".tecla_psh");
+const ticBtn = document.querySelector(".tecla_tic");
+const tomBtn = document.querySelector(".tecla_tom");
