32f4d56dafcd39233dc63c0221bc024af4103e37
Pablo Quiroga <quirogalogins@gmail.com>
2025-01-08 10:01:14 -0300

    Eventos no teclado;Condições no código e operadores lógicos;Mais condições

---
+++ 05-javascript/aula-05/main.js
@@ -0,0 +1,32 @@
+const listaDeTeclas = document.querySelectorAll(".tecla");
+let contador = 0;
+
+function tocaSom(seletorAudio) {
+  const elemento = document.querySelector(seletorAudio);
+
+  if (elemento.localName === "audio") {
+    elemento.play();
+  } else {
+    console.log("Elemento não encontrado.");
+  }
+}
+
+for (let contador = 0; contador < listaDeTeclas.length; contador++) {
+  const tecla = listaDeTeclas[contador];
+  const instrumento = tecla.classList[1];
+  const idAudio = `#som_${instrumento}`;
+
+  tecla.onclick = function () {
+    tocaSom(idAudio);
+  };
+
+  tecla.onkeydown = function (evento) {
+    if (evento.code === "Space" || evento.code === "Enter") {
+      tecla.classList.add("ativa");
+    }
+  };
+
+  tecla.onkeyup = function () {
+    tecla.classList.remove("ativa");
+  };
+}
