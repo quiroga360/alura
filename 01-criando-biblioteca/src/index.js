9bb91c2cf530e6294c6753fb86542fef9f5bab3a
Pablo Quiroga <quirogalogins@gmail.com>
2025-01-22 06:06:20 -0300

    Como utilizar métodos de array e objeto do JavaScript para resolver problemas comuns de lógica de programação, como manipulação de arrays, strings e objetos; Como organizar as funcionalidades do projeto em funções separadas, utilizando a importação e exportação de módulos para comunicar as funções entre si e utilizá-las onde necessário.

--- 01-criando-biblioteca/src/index.js
+++ 01-criando-biblioteca/src/index.js
@@ -7,5 +7,40 @@ const link = caminhoArquivo[2];
 
 // ler arquivo
 fs.readFile(link, "UTF-8", (erro, texto) => {
-  console.log(texto);
+  //verificaPalavrasDuplicadas(texto);
+  quebraEmParagrafos(texto);
 });
+
+// criar um array de palavras
+// contar as ocorrências
+// montar um objeto com o resultado
+
+function quebraEmParagrafos(textoInput) {
+  const paragrafos = textoInput.toLowerCase().split("\n");
+  const contagem = paragrafos.flatMap((paragrafo) => {
+    if (!paragrafo) return [];
+    return verificaPalavrasDuplicadas(paragrafo);
+  });
+
+  console.log(contagem);
+}
+
+function limpaPalavras(palavra) {
+  return palavra.replace(/[.,\/#!$%\^&\*;:{}=\-_`~()]/g, "");
+}
+
+function verificaPalavrasDuplicadas(textoInput) {
+  const listaPalavras = textoInput.split(" ");
+  const resultado = {};
+
+  listaPalavras.forEach((palavra) => {
+    if (palavra.length >= 3) {
+      const palavraLimpa = limpaPalavras(palavra);
+      resultado[palavraLimpa] = (resultado[palavraLimpa] || 0) + 1;
+    }
+  });
+
+  console.log(resultado);
+}
+
+// continuar em separando em parágrafos
